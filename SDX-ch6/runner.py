95ba63df83fef8d1e1c3588a40bc6f9f4daf3538
Coden Stark <codenstark@gmail.com>
2024-02-06 13:59:30 -0800

    exercise 2 done

--- SDX-ch6/runner.py
+++ SDX-ch6/runner.py
@@ -1,12 +1,12 @@
 import unittest
+import pprint
 
 from sign_func import sign
 # unittest must import what we want to test, can't be in the same file (like pytest).
 
 # pytest has boolean stuff, unittest is one function/method, more concise.
 
-    
-class TestCade(unittest.TestCase):
+class TestCase(unittest.TestCase):
     def test_sign_negative(self):
         self.assertEqual(-1,sign(-3))
 
@@ -14,24 +14,25 @@ class TestCade(unittest.TestCase):
         self.assertEqual(1,sign(19))
 
     def test_sign_zero(self):
-        self.assertEqual(1,sign(0))
+        self.assertTrue(1,sign(0))
 
     def test_sign_error(self):
-        self.assertFalse(1,sign(1))
+        with self.assertRaises(AssertionError):
+            self.assertFalse(1,sign(1))
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
 
 
+'''
 # [run]
-    '''
-        def run_tests():
+def run_tests():
     results = {"pass": 0, "fail": 0, "error": 0}
     for (name, test) in globals().items():
         if not name.startswith("test_"):
             continue
         try:
-            test()
+            TestCase()
             results["pass"] += 1
         except AssertionError:
             results["fail"] += 1
