a4c1ae9d2bd5f04bc7cee914fa567b2f3415c893
gabepm100 <gabepm100@gmail.com>
2024-02-06 13:48:26 -0800

    Terence and Gabe 1st commit

--- SDX-ch6/manual.py
+++ SDX-ch6/manual.py
@@ -17,7 +17,7 @@ def test_sign_zero():
     assert sign(0) == 0
 
 def test_sign_error():
-    assert sgn(1) == 1
+    assert sign(1) == 1
 # [/tests]
 
 # [run]
--- SDX-ch6/runner.py
+++ SDX-ch6/runner.py
@@ -1,3 +1,5 @@
+import unittest
+
 def sign(value):
     if value < 0:
         return -1
@@ -14,9 +16,32 @@ def test_sign_zero():
     assert sign(0) == 0
 
 def test_sign_error():
-    assert sgn(1) == 1
+    assert sign(1) == 1
 
 # [run]
+class TestCase(unittest.TestCase):
+    def test_sign_negative(self):
+        self.assertEqual(sign(-3),-1)
+
+    def test_sign_positive(self):
+        self.assertEqual(sign(19),1)
+
+    def test_sign_zero(self):
+        self.assertEqual(sign(0),0)
+
+    def test_sign_error(self):
+        self.assertEqual(sign(1),1)
+
+
+# Test.test_sign_error()
+# Test.test_sign_negative()
+# Test.test_sign_positive()
+# Test.test_sign_zero()
+
+def runtests():
+    Test=TestCase()
+    unittest.main()
+
 def run_tests():
     results = {"pass": 0, "fail": 0, "error": 0}
     for (name, test) in globals().items():
@@ -29,9 +54,11 @@ def run_tests():
             results["fail"] += 1
         except Exception:
             results["error"] += 1
+
     print(f"pass {results['pass']}")
     print(f"fail {results['fail']}")
     print(f"error {results['error']}")
 # [/run]
 
 run_tests()
+runtests()
