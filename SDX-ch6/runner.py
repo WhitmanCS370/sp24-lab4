c369fb0b4cd98480537313bfa4bc1ae044570020
didwayg <75285447+didwayg@users.noreply.github.com>
2024-02-06 14:11:19 -0800

    Compiled and organized edits in runner.py

--- SDX-ch6/runner.py
+++ SDX-ch6/runner.py
@@ -1,37 +1,24 @@
-def sign(value):
-    if value < 0:
-        return -1
-    else:
-        return 1
+# Improved complete runner using a testing class inheriting from the unittest module
+import unittest
+from sign import sign
 
-def test_sign_negative():
-    assert sign(-3) == -1
 
-def test_sign_positive():
-    assert sign(19) == 1
+class TestClass(unittest.TestCase):
+    def test_sign_negative(self):
+        self.assertEqual(-1, sign(-3))
 
-def test_sign_zero():
-    assert sign(0) == 0
+    def test_sign_positive(self):
+        self.assertEqual(1, sign(19))
 
-def test_sign_error():
-    assert sgn(1) == 1
+    def test_sign_zero(self):
+        self.assertEqual(1, sign(0))
 
-# [run]
-def run_tests():
-    results = {"pass": 0, "fail": 0, "error": 0}
-    for (name, test) in globals().items():
-        if not name.startswith("test_"):
-            continue
-        try:
-            test()
-            results["pass"] += 1
-        except AssertionError:
-            results["fail"] += 1
-        except Exception:
-            results["error"] += 1
-    print(f"pass {results['pass']}")
-    print(f"fail {results['fail']}")
-    print(f"error {results['error']}")
-# [/run]
+    def test_sign_error(self):
+        self.assertEqual(1, sign(1))
 
-run_tests()
+    def test_exception(self):
+        with self.assertRaises(TypeError):
+            sign('0') 
+
+if __name__ == '__main__':
+    unittest.main()
